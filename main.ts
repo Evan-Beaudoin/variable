a559c0c416acf7391be7b4f9f4e71b5a09ba4dd2
Evan-Beaudoin <71400127+Evan-Beaudoin@users.noreply.github.com>
2020-09-28 12:58:24 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,11 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    number_change_on_button_A += 1
+    basic.showString("" + (number_change_on_button_A))
 })
+// Created By: Evan
+// 
+// Created on: Sept. 2020
+// 
+// This program: uses variable
+let number_change_on_button_A = 0
+number_change_on_button_A = 0
